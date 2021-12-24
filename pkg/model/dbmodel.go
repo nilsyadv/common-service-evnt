b36618b84b1bb880bfb544c4e41222234d23b08f
Nilesh-Coherent <Nilesh.Coherent@gmail.com>
2021-12-24 21:50:52 +0530

    db model changed

--- pkg/model/dbmodel.go
+++ pkg/model/dbmodel.go
@@ -9,8 +9,8 @@ import (
 
 // Trandb Used Under All DB Table For Latest Transaction Detail
 type Trandb struct {
-	ID        uuid.UUID `gorm:"primaryKey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uuid.UUID      `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
